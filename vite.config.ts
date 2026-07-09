1bff5999a7f4d3195934faec026b2b4147c08ad3
rhianpablo11 <rhianpablo11@gmail.com>
2026-07-09 17:19:22 -0300

    feat: add pwa dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,44 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(),,
+            VitePWA({
+      registerType: 'autoUpdate', // Atualiza o app sozinho quando você sobe versão nova
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'], // Arquivos estáticos
+      manifest: {
+        name: 'Onyx Finance', // Nome completo
+        short_name: 'Onyx', // Nome que fica embaixo do ícone no celular
+        description: 'Seu gerenciador financeiro pessoal',
+        theme_color: '#000000', // Cor da barra de status do celular
+        background_color: '#000000', // Cor de fundo enquanto carrega
+        display: 'standalone', // <--- ISSO TIRA A BARRA DO NAVEGADOR (Fica parecendo app nativo)
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait', // Trava em pé (opcional)
+        icons: [
+          {
+            src: 'android-chrome-192x192.png', // Você precisa criar esse arquivo (ver Passo 3)
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png', // Você precisa criar esse arquivo (ver Passo 3)
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable' // Importante pro Android (ícone redondinho)
+          }
+        ]
+      }
+    })],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
